577da8f6a8243a1a5ee03e8e58d51b01bea31ca3
Joeyoung <393098486@qq.com>
2018-07-16 10:20:19 +0800

    Initial Commit

---
+++ TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by 乔杨 on 2018/7/16.
+//  Copyright © 2018年 Joeyoung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestDemo
+//
+//  Created by 乔杨 on 2018/7/16.
+//  Copyright © 2018年 Joeyoung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
